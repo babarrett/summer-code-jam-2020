f1066abcf09d928707a5da1ef2b13741f3535630
Ryan Johnson <ryanj.johnson@gmail.com>
2020-08-09 00:17:02 -0700

    updated puffin_setup to check system.

--- proud-puffins/djangoProject/puffin_setup.py
+++ proud-puffins/djangoProject/puffin_setup.py
@@ -1,7 +1,43 @@
 import subprocess
 import os
 import sqlite3
+import platform
 
+if platform.system() == "Windows":
+    # Moves thoughout the directory loading the files as needed to to run the app.
+
+    os.chdir("../")
+    subprocess.run(["pip", "install", "-r", "requirements.txt"], shell=True)
+    os.chdir("./djangoProject")
+    subprocess.run(["python", "manage.py", "makemigrations"], shell=True)
+    subprocess.run(["python", "manage.py", "migrate"], shell=True)
+    subprocess.run(["python", "manage.py", "loaddata", "users.json"], shell=True)
+
+    try:
+        subprocess.run(["python", "manage.py", "loaddata", "profiles.json"], shell=True)
+
+    except:
+        tablewipe()
+        subprocess.run(["python", "manage.py", "loaddata", "profiles.json"], shell=True)
+
+    subprocess.run(["python", "manage.py", "runserver"], shell=True)
+
+else:
+    os.chdir("../")
+    subprocess.run(["pip", "install", "-r", "requirements.txt"])
+    os.chdir("./djangoProject")
+    subprocess.run(["python", "manage.py", "makemigrations"])
+    subprocess.run(["python", "manage.py", "migrate"])
+    subprocess.run(["python", "manage.py", "loaddata", "users.json"])
+
+    try:
+        subprocess.run(["python", "manage.py", "loaddata", "profiles.json"])
+
+    except:
+        tablewipe()
+        subprocess.run(["python", "manage.py", "loaddata", "profiles.json"])
+
+    subprocess.run(["python", "manage.py", "runserver"])
 
 # If an error happens during loading data, empties the table and returns.
 def tablewipe():
@@ -17,23 +53,3 @@ def tablewipe():
     conn.commit()
     # close the connection
     conn.close()
-
-
-# Moves thoughout the directory loading the files as needed to to run the app.
-
-os.chdir("../")
-subprocess.run(["pip", "install", "-r", "requirements.txt"], shell=True)
-os.chdir("./djangoProject")
-subprocess.run(["python", "manage.py", "makemigrations"], shell=True)
-subprocess.run(["python", "manage.py", "migrate"], shell=True)
-subprocess.run(["python", "manage.py", "loaddata", "users.json"], shell=True)
-
-try:
-    subprocess.run(["python", "manage.py", "loaddata", "profiles.json"], shell=True)
-
-except:
-    tablewipe()
-    subprocess.run(["python", "manage.py", "loaddata", "profiles.json"], shell=True)
-
-
-subprocess.run(["python", "manage.py", "runserver"], shell=True)
